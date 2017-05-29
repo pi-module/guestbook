e717aa758e9385ec56416f75bd924c4dcbe991b9
voltan <djvoltan@gmail.com>
2017-05-29 09:59:37 +0430

    Add breadcrumb

---
+++ src/Api/Breadcrumbs.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Pi Engine (http://pialog.org)
+ *
+ * @link            http://code.pialog.org for the Pi Engine source repository
+ * @copyright       Copyright (c) Pi Engine http://pialog.org
+ * @license         http://pialog.org/license.txt New BSD License
+ */
+
+/**
+ * @author Hossein Azizabadi <azizabadi@faragostaresh.com>
+ */
+namespace Module\Guestbook\Api;
+
+use Pi;
+use Pi\Application\Api\AbstractBreadcrumbs;
+
+class Breadcrumbs extends AbstractBreadcrumbs
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load()
+    {
+        // Set module link
+        $moduleData = Pi::registry('module')->read($this->getModule());
+        $result = array();
+        // Set module internal links
+        $result[] = array(
+            'label' => $moduleData['title'],
+        );
+        // return
+        return $result;
+    }
+}
